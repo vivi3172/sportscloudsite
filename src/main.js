457308323b91b3033bba0f29caa4ea21e8cf253a
Liu vivi <vivi@ebmtech.com>
2023-08-04 23:13:21 +0800

    修正

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Amplify from 'aws-amplify';
-import '@aws-amplify/ui-vue';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
 
 createApp(App).mount('#app')
